963b47bb157de4e890530311fe5f9b35b86f231c
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2025-10-22 13:47:36 +0000

    Cleanup

--- src/cosine.cc
+++ src/cosine.cc
@@ -1,31 +1,17 @@
 #include <G4GenericMessenger.hh>
-#include <G4PrimaryParticle.hh>
-#include <G4String.hh>
 #include <G4SystemOfUnits.hh>   // physical units such as `m` for metre
 #include <G4Event.hh>           // needed to inject primary particles into an event
-#include <G4Box.hh>             // for creating shapes in the geometry
-#include <G4Sphere.hh>          // for creating shapes in the geometry
 #include <FTFP_BERT.hh>         // our choice of physics list
 
 #include <n4-all.hh>
 
-#include <G4ThreeVector.hh>
 #include <cstdlib>
 
 #include "generators/geantinos.hh"
 #include "actions/store_volume_crossing.hh"
 #include "geometry/pcolina.hh"
 
-struct my {
-  G4double       straw_radius{0.1 * m};
-  G4double      bubble_radius{0.2 * m};
-  G4double      socket_rot   {-90 * deg};
-  G4String      particle_name{"geantino"};
-  G4double      particle_energy{511 * keV};
-  G4ThreeVector particle_dir {};
-};
-
-n4::actions* create_actions(my& my, unsigned& n_event) {
+n4::actions* create_actions(unsigned& n_event) {
   auto my_event_action = [&] (const G4Event*) {
      n_event++;
      std::cout << "end of event " << n_event << std::endl;
@@ -39,42 +25,26 @@ n4::actions* create_actions(my& my, unsigned& n_event) {
 int main(int argc, char* argv[]) {
   unsigned n_event = 0;
 
-  my my;
-
   G4int physics_verbosity = 0;
 
   // The trailing slash after '/my_geometry' is CRUCIAL: without it, the
   // messenger violates the principle of least surprise.
   auto messenger = new G4GenericMessenger{nullptr, "/my/", "docs: bla bla bla"};
-  messenger -> DeclarePropertyWithUnit("straw_radius"      , "m"  , my. straw_radius  );
-  messenger -> DeclarePropertyWithUnit("bubble_radius"     , "m"  , my.bubble_radius  );
-  messenger -> DeclarePropertyWithUnit("socket_rot"        , "deg", my.socket_rot     );
-  messenger -> DeclarePropertyWithUnit("particle_energy"   , "keV", my.particle_energy);
-  messenger -> DeclareProperty        ("particle"          ,        my.particle_name  );
-  messenger -> DeclareProperty        ("particle_direction",        my.particle_dir   );
   messenger -> DeclareProperty        ("physics_verbosity" ,        physics_verbosity );
 
   n4::run_manager::create()
     .ui("cosine", argc, argv)
     .macro_path("macs")
-    .apply_command("/my/straw_radius 0.5 m")
-    .apply_early_macro("early-hard-wired.mac")
     .apply_cli_early() // CLI --early executed at this point
-    // .apply_command(...) // also possible after apply_early_macro
 
+    // Important! physics list has to be set before the generator!
     .physics<FTFP_BERT>(physics_verbosity)
     .geometry(pcolina)
-    .actions(create_actions(my, n_event))
+    .actions(create_actions(n_event))
 
-    .apply_command("/my/particle e-")
-    .apply_late_macro("late-hard-wired.mac")
     .apply_cli_late() // CLI --late executed at this point
-    // .apply_command(...) // also possible after apply_late_macro
 
     .run();
 
-
-
-  // Important! physics list has to be set before the generator!
 
 }

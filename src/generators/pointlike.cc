2039603a13f8bee9895b5ef45a637a5fc0862f86
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2025-10-29 13:40:41 +0000

    Add pointlike generator

---
+++ src/generators/pointlike.cc
@@ -0,0 +1,27 @@
+#include "generators/pointlike.hh"
+#include "G4DynamicParticle.hh"
+#include "G4PrimaryVertex.hh"
+#include "G4ThreeVector.hh"
+#include <n4-inspect.hh>
+
+pointlike::pointlike(const G4String& particle_name, G4ThreeVector pos, u16 nparticles, G4double energy)
+  : particle_(n4::find_particle(particle_name))
+  , pos_(pos)
+  , energy_(energy)
+  , npart_(nparticles)
+  , polarization_()
+{}
+
+G4PrimaryVertex* pointlike::generate_vertex() const {
+  auto vertex = new G4PrimaryVertex(pos_, 0.);
+  for (auto i = 0; i < npart_; i++) {
+    auto dir = generate_direction();
+    auto pol = generate_polarization();
+    auto ene = generate_energy();
+    auto particle = new G4PrimaryParticle(particle_, dir.x(), dir.y(), dir.z(), ene);
+
+    particle -> SetPolarization(pol);
+    vertex   -> SetPrimary(particle);
+  }
+  return vertex;
+}

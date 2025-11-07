08079d08c4978813cf933042f093c10798777a0c
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2025-11-07 11:44:36 +0000

    Add PTFE surface

--- src/materials/ptfe.cc
+++ src/materials/ptfe.cc
@@ -1,27 +1,35 @@
+#include "G4OpticalSurface.hh"
 #include "common.hh"
 #include "materials/ptfe.hh"
 
 #include <n4-material.hh>
 #include <n4-sequences.hh>
 
-G4Material* ptfe_with_properties() {
-  auto teflon = n4::material("G4_TEFLON");
-
+G4MaterialPropertiesTable* ptfe_mpt() {
   // Values could be taken from "Optical properties of Teflon AF amorphous
   // fluoropolymers" by Yang, French & Tokarsky (using AF2400, Fig.6) but are
   // also stated in the same paper as above
-  auto refl_energies = n4::scale_by(  eV, {OPTPHOT_MIN_E_EV, 2.8, 3.5, 4., 6., 7.2, OPTPHOT_MAX_E_EV});
   auto refl_values   = n4::scale_by(0.01, {              98,  98,  98, 98, 72,  72,              72 });
+  auto refl_energies = n4::scale_by(  eV, {OPTPHOT_MIN_E_EV, 2.8, 3.5, 4., 6., 7.2, OPTPHOT_MAX_E_EV});
   auto ref_index     = 1.35; // 1.41 in nexus
 
-  auto mpt = n4::material_properties()
-    .add("RINDEX", OPTPHOT_E_RANGE, ref_index)
-    .add("REFLECTIVITY", refl_energies, refl_values)
-    .add("SPECULARLOBECONSTANT" , OPTPHOT_E_RANGE, 0.)
-    .add("SPECULARSPIKECONSTANT", OPTPHOT_E_RANGE, 0.)
-    .add("BACKSCATTERCONSTANT"  , OPTPHOT_E_RANGE, 0.)
-    .done();
+  return n4::material_properties()
+      .add("RINDEX", OPTPHOT_E_RANGE, ref_index)
+      .add("REFLECTIVITY", refl_energies, refl_values)
+      .add("SPECULARLOBECONSTANT", OPTPHOT_E_RANGE, 0.)
+      .add("SPECULARSPIKECONSTANT", OPTPHOT_E_RANGE, 0.)
+      .add("BACKSCATTERCONSTANT", OPTPHOT_E_RANGE, 0.)
+      .done();
+}
 
-  teflon -> SetMaterialPropertiesTable(mpt);
+G4Material* ptfe_with_properties() {
+  auto teflon = n4::material("G4_TEFLON");
+  teflon -> SetMaterialPropertiesTable(ptfe_mpt());
   return teflon;
+}
+
+G4OpticalSurface* ptfe_surface() {
+  auto surface = new G4OpticalSurface("ptfe surface", unified, ground, dielectric_metal, .01);
+  surface -> SetMaterialPropertiesTable(ptfe_mpt());
+  return surface;
 }
--- src/materials/ptfe.hh
+++ src/materials/ptfe.hh
@@ -1,5 +1,9 @@
 #pragma once
 
 #include <G4Material.hh>
+#include <G4MaterialPropertiesTable.hh>
+#include <G4OpticalSurface.hh>
 
-G4Material *ptfe_with_properties();
+G4MaterialPropertiesTable* ptfe_mpt();
+G4Material*                ptfe_with_properties();
+G4OpticalSurface*          ptfe_surface();

9261b1be84e1692344d5f3ae059e4c141b3d5331
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2025-11-12 09:53:30 +0000

    Fix scalar sampling

--- src/generators/scalar.cc
+++ src/generators/scalar.cc
@@ -1,25 +1,32 @@
 #include "generators/scalar.hh"
 #include "materials/LXe.hh"
+#include "types.hh"
 
 #include <G4SystemOfUnits.hh>
+
+#include <n4-constants.hh>
 #include <n4-random.hh>
 #include <n4-sequences.hh>
+
+#include <cassert>
 #include <numeric>
 
 
 lxe_scintillation::lxe_scintillation()
 {
-  auto n = 100;
-  energy_ = n4::linspace(165 * nm, 185 * nm, n);
+  auto n   = 101;
+  auto wls = n4::linspace(165 * nm, 185 * nm, n);
+  energy_  = n4::map<f64>([](f64 wl) {return c4::hc/wl;}, wls);
 
-  auto pdf = n4::map<f64>(LXe_Scintillation, energy_);
+  auto pdf    = n4::map<f64>(LXe_Scintillation, energy_);
+  auto pdfsum = std::accumulate(pdf.cbegin(), pdf.cend(), 0.);
+  assert((pdfsum > 0) && "PDF does not have a strictly positive sum");
+
   cdf_.reserve(n);
+  cdf_.push_back(pdf[0] / pdfsum);
+  for (auto i=1; i<pdf.size(); i++)
+    cdf_.push_back(cdf_[i-1] + pdf[i] / pdfsum);
 
-  auto pdfsum = std::accumulate(pdf.cbegin(), pdf.cend(), 0);
-  cdf_[0] = pdf[0] / pdfsum;
-  for (auto i=1; i<pdf.size(); i++) {
-    cdf_[i] = cdf_[i-1] + pdf[i] / pdfsum;
-  }
   half_bin_size = (energy_[1] - energy_[0]) / 2;
 }
 

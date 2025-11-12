5e3b51d861ce3a842691b4124754dd05063052a2
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2025-11-12 09:36:21 +0000

    Improve photon detection in sipms

--- src/sensitive/sipm.cc
+++ src/sensitive/sipm.cc
@@ -1,13 +1,17 @@
-#include "G4HCofThisEvent.hh"
+
 #include "common.hh"
+#include "materials/silicon.hh"
 #include "types.hh"
 #include "sensitive/sipm.hh"
 
+#include <G4HCofThisEvent.hh>
 #include <G4OpticalPhoton.hh>
 #include <G4ParticleDefinition.hh>
 #include <G4Step.hh>
 #include <G4VTouchable.hh>
+
 #include <n4-inspect.hh>
+#include <n4-random.hh>
 #include <n4-sensitive.hh>
 
 G4int get_sensor_id(const G4VTouchable* touch) {
@@ -26,15 +30,28 @@ std::unique_ptr<n4::sensitive_detector> sensitive_sipm() {
   auto reset_hit_store = [](G4HCofThisEvent*) { SENSOR_HITS.clear(); };
 
   auto record_hits = [](G4Step *step) -> bool {
-    static G4ParticleDefinition* photon = dynamic_cast<G4ParticleDefinition*>(G4OpticalPhoton::Definition());
+    static auto photondef = dynamic_cast<G4ParticleDefinition*>(G4OpticalPhoton::Definition());
 
     auto pdef = step -> GetTrack() -> GetDefinition();
-    if (pdef != photon) return false;
+    if (pdef != photondef) return false;
 
-    auto event = n4::event_number();
+    auto process = step -> GetPostStepPoint()
+                        -> GetProcessDefinedStep()
+                        -> GetProcessName();
+    if (process != "OpAbsorption") return false;
+
+    // Simulate detection quantum efficiency
+    static const auto& sipm_qe = silicon_mpt()->GetProperty("QUANTUM_EFFICIENCY");
+
+    auto photon_e = step -> GetTrack() -> GetDynamicParticle() -> GetTotalEnergy();
+    auto qe       = sipm_qe -> Value(photon_e);
+    if (n4::random::uniform() > qe) return false;
+
+    auto event     = n4::event_number();
     auto sensor_id = get_sensor_id(step -> GetPostStepPoint() -> GetTouchable());
-    auto time = step -> GetPostStepPoint() -> GetGlobalTime();
+    auto time      = step -> GetPostStepPoint() -> GetGlobalTime();
     SENSOR_HITS.push_back(make_sensor_hit(event, sensor_id, time));
+
     return true;
   };
 

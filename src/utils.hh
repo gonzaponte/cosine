839e19a9305591518741a1bdc13d60ff5021eecf
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2025-11-12 20:12:25 +0000

    Add zip

--- src/utils.hh
+++ src/utils.hh
@@ -13,11 +13,36 @@
   }
 
 
-template <class T>
+template<class T>
 T intpow(T base, u16 exp) {
   T out = base;
   while (--exp > 0) out *= base;
   return out;
+}
+
+template <typename T1, typename T2>
+const auto zip(const std::vector<T1> &it1, const std::vector<T2> &it2) {
+  using V1 = std::vector<T1>;
+  using V2 = std::vector<T2>;
+  using I1 = decltype(std::cbegin(std::declval<V1>()));
+  using I2 = decltype(std::cbegin(std::declval<V2>()));
+
+  struct zip_iter {
+    I1 i1;
+    I2 i2;
+
+    bool operator != (const zip_iter& other) const { return this->i1 != other.i1; }
+    void operator ++ ()       { ++i1; ++i2; }
+    auto operator *  () const { return std::make_pair<const T1&, const T2&>(*i1, *i2); }
+  };
+
+  struct zip_wrapper {
+    const V1& v1;
+    const V2& v2;
+    auto begin() const { return zip_iter{std::begin(v1), std::begin(v2)}; }
+    auto end  () const { return zip_iter{std::end  (v1), std::end  (v2)}; }
+  };
+  return zip_wrapper{it1, it2};
 }
 
 template <class T, typename... Args>

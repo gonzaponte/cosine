21f148a23e7fa2939293e37bc91805df7388eb04
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2025-11-13 19:26:44 +0000

    Rename local variable

--- src/geometry/pcolina.cc
+++ src/geometry/pcolina.cc
@@ -19,13 +19,13 @@
 
 #include "config.hh"
 
-auto pcolina(const geometry_config& c) {
-  auto full_neck_length = c.neck_length
-                        + c.mesh_thick // gate thickness
-                        + c.d_gate_wire   - c.frame_thick_wires/2
-                        + c.d_wire_shield - c.frame_thick_wires/2
-                        + c.mesh_thick // shield thickness
-                        + c.d_shield_sipms;
+auto pcolina(const geometry_config& g) {
+  auto full_neck_length = g.neck_length
+                        + g.mesh_thick // gate thickness
+                        + g.d_gate_wire   - g.frame_thick_wires/2
+                        + g.d_wire_shield - g.frame_thick_wires/2
+                        + g.mesh_thick // shield thickness
+                        + g.d_shield_sipms;
 
   auto air   = n4::material("G4_AIR");
   auto lxe   =   LXe_with_properties();
@@ -44,14 +44,14 @@ auto pcolina(const geometry_config& c) {
   //n4::place::check_overlaps_switch_on();
 
   auto world = n4::box("world")
-    .cube(1.2 * c.cath_diam())
-    .z(2.5 * c.drift_length)
+    .cube(1.2 * g.cath_diam())
+    .z(2.5 * g.drift_length)
     .vis(frame)
     .volume(air);
 
-  auto liquid_length = (c.drift_length + full_neck_length) * 2.1;
+  auto liquid_length = (g.drift_length + full_neck_length) * 2.1;
   auto liquid = n4::box("liquid")
-    .cube(1.1 * c.cath_diam())
+    .cube(1.1 * g.cath_diam())
     .z(liquid_length)
     .vis(red)
     .place(lxe)
@@ -59,34 +59,34 @@ auto pcolina(const geometry_config& c) {
     .now();
 
   auto neck = n4::tubs("neck")
-    .r_inner(c.el_r())
-    .r_delta(c.wall_thick)
-    .z(c.neck_length)
+    .r_inner(g.el_r())
+    .r_delta(g.wall_thick)
+    .z(g.neck_length)
     .vis(white)
     .place(ptfe)
     .in(liquid)
-    .at_z(c.neck_length/2)
+    .at_z(g.neck_length/2)
     .now();
 
   new G4LogicalSkinSurface("neck_surface", neck -> GetLogicalVolume(), ptfe_surface());
 
 
   // Field cage rings
-  for (auto i=0; i<c.fc_rings; i++) {
+  for (auto i=0; i<g.fc_rings; i++) {
     char name[20];
-    auto z  = c.fc_ring_zpitch*i + c.fc_ring_width/2;
-    auto r1 = c.el_r() + c.form_factor * (z - c.fc_ring_width/2);
-    auto r2 = c.el_r() + c.form_factor * (z + c.fc_ring_width/2);
+    auto z  = g.fc_ring_zpitch*i + g.fc_ring_width/2;
+    auto r1 = g.el_r() + g.form_factor * (z - g.fc_ring_width/2);
+    auto r2 = g.el_r() + g.form_factor * (z + g.fc_ring_width/2);
     std::sprintf(name, "field_cage_ring_%d", i);
     auto ring = n4::cons(name)
       .r1_inner(r1)
       .r2_inner(r2)
-      .r_delta(c.fc_ring_thick)
-      .z(c.fc_ring_width)
+      .r_delta(g.fc_ring_thick)
+      .z(g.fc_ring_width)
       .vis(gray)
       .place(steel)
       .in(liquid)
-      .at_z(c.neck_length + z)
+      .at_z(g.neck_length + z)
       .now();
 
     char name2[30];
@@ -94,47 +94,47 @@ auto pcolina(const geometry_config& c) {
     new G4LogicalSkinSurface(name2, ring -> GetLogicalVolume(), steel_surface());
   }
 
-  if (c.ptfe_on_walls) {
+  if (g.ptfe_on_walls) {
     auto ptfe_walls = n4::cons("ptfe_walls")
-      .r1_inner(c.el_r())
-      .r2_inner(c.cath_r())
-      .r_delta(c.wall_thick)
-      .z(c.drift_length)
+      .r1_inner(g.el_r())
+      .r2_inner(g.cath_r())
+      .r_delta(g.wall_thick)
+      .z(g.drift_length)
       .vis(wireframe)
       .place(ptfe)
       .in(liquid)
-      .at_z(c.neck_length + c.drift_length/2)
+      .at_z(g.neck_length + g.drift_length/2)
       .now();
 
     new G4LogicalSkinSurface("cath_surface", ptfe_walls -> GetLogicalVolume(), ptfe_surface());
   }
 
-  auto wire_array = create_wire_array(c);
+  auto wire_array = create_wire_array(g);
 
   wire_array -> SetVisAttributes(gray);
   n4::place(wire_array)
-    .rot_z(c.thin_wire_rot)
-    .at_z(-c.mesh_thick - c.d_gate_wire)
+    .rot_z(g.thin_wire_rot)
+    .at_z(-g.mesh_thick - g.d_gate_wire)
     .in(liquid)
     .now();
 
-  auto mesh_el = create_hex_mesh( c.el_diam
-                                , c.frame_thick_mesh
-                                , c.frame_width
-                                , c.mesh_hex_pitch
-                                , c.mesh_thick
-                                , c.mesh_hex_inradius);
+  auto mesh_el = create_hex_mesh( g.el_diam
+                                , g.frame_thick_mesh
+                                , g.frame_width
+                                , g.mesh_hex_pitch
+                                , g.mesh_thick
+                                , g.mesh_hex_inradius);
   mesh_el -> SetVisAttributes(invisible);
   // mesh_el -> SetVisAttributes(green);
 
-  n4::place(mesh_el).name("gate"  ).at_z(-c.mesh_thick/2                                              ).in(liquid).now();
-  n4::place(mesh_el).name("shield").at_z(-c.mesh_thick -c.d_gate_wire -c.d_wire_shield -c.mesh_thick/2).in(liquid).now();
+  n4::place(mesh_el).name("gate"  ).at_z(-g.mesh_thick/2                                              ).in(liquid).now();
+  n4::place(mesh_el).name("shield").at_z(-g.mesh_thick -g.d_gate_wire -g.d_wire_shield -g.mesh_thick/2).in(liquid).now();
 
-  auto z_cathode = c.neck_length + c.drift_length + c.cath_thick/2;
-  z_cathode += c.ptfe_on_fp ? c.wall_thick : 0;
+  auto z_cathode = g.neck_length + g.drift_length + g.cath_thick/2;
+  z_cathode += g.ptfe_on_fp ? g.wall_thick : 0;
   auto cathode = n4::tubs("cathode")
-    .r(c.cath_r() + c.wall_thick)
-    .z(c.cath_thick)
+    .r(g.cath_r() + g.wall_thick)
+    .z(g.cath_thick)
     .vis(gray)
     .place(steel)
     .at_z(z_cathode)
@@ -143,35 +143,35 @@ auto pcolina(const geometry_config& c) {
 
   new G4LogicalSkinSurface("cathode_surface", cathode -> GetLogicalVolume(), steel_surface());
 
-  if (c.ptfe_on_fp) {
+  if (g.ptfe_on_fp) {
     auto ptfe_cathode = n4::tubs("ptfe_cathode")
-      .r(c.cath_r() + c.wall_thick)
-      .z(c.wall_thick)
+      .r(g.cath_r() + g.wall_thick)
+      .z(g.wall_thick)
       .vis(white)
       .place(ptfe)
-      .at_z(z_cathode -c.cath_thick/2 -c.wall_thick/2)
+      .at_z(z_cathode -g.cath_thick/2 -g.wall_thick/2)
       .in(liquid)
       .now();
 
     new G4LogicalSkinSurface("cath_surface", ptfe_cathode -> GetLogicalVolume(), ptfe_surface());
   }
 
-  auto sipm_array = build_sipm_array(c.sipm_size, c.sipm_thick, c.sipm_gap, c.n_sipm_side);
+  auto sipm_array = build_sipm_array(g.sipm_size, g.sipm_thick, g.sipm_gap, g.n_sipm_side);
   n4::place(sipm_array)
-    .at_z(-full_neck_length -c.sipm_thick)
+    .at_z(-full_neck_length -g.sipm_thick)
     .in(liquid)
     .copy_no(0)
     .name("near_plane")
     .now();
 
-  if (c.sipms_on_fp) {
-    auto delta = (c.sipm_size + c.sipm_gap) * c.n_sipm_side / 2.;
+  if (g.sipms_on_fp) {
+    auto delta = (g.sipm_size + g.sipm_gap) * g.n_sipm_side / 2.;
     auto n = 1;
     for   (auto x : {-delta, delta}) {
       for (auto y : {-delta, delta}) {
         n4::place(sipm_array)
           .rot_y(180 * deg)
-          .at(x, y, -c.cath_thick/2 + c.sipm_thick)
+          .at(x, y, -g.cath_thick/2 + g.sipm_thick)
           .in(cathode)
           .copy_no(n++)
           .name("far_plane")

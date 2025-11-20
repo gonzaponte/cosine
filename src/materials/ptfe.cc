9213bb53ebcf51e0ba594fde508c4dc5aa553b2c
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2025-11-20 08:45:59 +0000

    Adjust PTFE reflectivity

--- src/materials/ptfe.cc
+++ src/materials/ptfe.cc
@@ -9,13 +9,14 @@ G4MaterialPropertiesTable* ptfe_mpt() {
   // Values could be taken from "Optical properties of Teflon AF amorphous
   // fluoropolymers" by Yang, French & Tokarsky (using AF2400, Fig.6) but are
   // also stated in the same paper as above
-  auto refl_values   = n4::scale_by(0.01, {              98,  98,  98, 98, 72,  72,              72 });
+  auto refl_values = n4::scale_by(0.01, {98, 98, 98, 98, 72, 72, 72});
+  auto refl_fixed = 0.97;
   auto refl_energies = n4::scale_by(  eV, {OPTPHOT_MIN_E_EV, 2.8, 3.5, 4., 6., 7.2, OPTPHOT_MAX_E_EV});
-  auto ref_index     = 1.35; // 1.41 in nexus
+  auto ref_index     = 1.58; // 1.35 in other projects, 1.41 in nexus
 
   return n4::material_properties()
       .add("RINDEX", OPTPHOT_E_RANGE, ref_index)
-      .add("REFLECTIVITY", refl_energies, refl_values)
+      .add("REFLECTIVITY", refl_energies, refl_fixed)
       .add("SPECULARLOBECONSTANT", OPTPHOT_E_RANGE, 0.)
       .add("SPECULARSPIKECONSTANT", OPTPHOT_E_RANGE, 0.)
       .add("BACKSCATTERCONSTANT", OPTPHOT_E_RANGE, 0.)

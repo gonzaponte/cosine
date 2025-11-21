0226a2d889f896c4ef069a520eab7eabc3575827
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2025-11-21 08:28:06 +0000

    Generalize zip function

--- src/utils.hh
+++ src/utils.hh
@@ -21,11 +21,9 @@ T intpow(T base, u16 exp) {
 }
 
 template <typename T1, typename T2>
-const auto zip(const std::vector<T1> &it1, const std::vector<T2> &it2) {
-  using V1 = std::vector<T1>;
-  using V2 = std::vector<T2>;
-  using I1 = decltype(std::cbegin(std::declval<V1>()));
-  using I2 = decltype(std::cbegin(std::declval<V2>()));
+const auto zip(T1&& it1, T2&& it2) {
+  using I1 = decltype(std::begin(std::declval<T1>()));
+  using I2 = decltype(std::begin(std::declval<T2>()));
 
   struct zip_iter {
     I1 i1;
@@ -33,16 +31,16 @@ const auto zip(const std::vector<T1> &it1, const std::vector<T2> &it2) {
 
     bool operator != (const zip_iter& other) const { return this->i1 != other.i1; }
     void operator ++ ()       { ++i1; ++i2; }
-    auto operator *  () const { return std::make_pair<const T1&, const T2&>(*i1, *i2); }
+    auto operator *  () const { return std::tie(*i1, *i2); }
   };
 
   struct zip_wrapper {
-    const V1& v1;
-    const V2& v2;
-    auto begin() const { return zip_iter{std::begin(v1), std::begin(v2)}; }
-    auto end  () const { return zip_iter{std::end  (v1), std::end  (v2)}; }
+    T1 v1;
+    T2 v2;
+    auto begin() const { return zip_iter{ std::begin(v1), std::begin(v2)}; }
+    auto end  () const { return zip_iter{ std::end  (v1), std::end  (v2)}; }
   };
-  return zip_wrapper{it1, it2};
+  return zip_wrapper{ std::forward<T1>(it1), std::forward<T2>(it2) };
 }
 
 template <class T, typename... Args>

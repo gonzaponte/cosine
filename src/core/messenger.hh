5e747ca21c63abaec05676a38ad94d8177d562ee
Gonzalo Martínez Lema <gonzaponte@gmail.com>
2026-04-16 16:02:05 +0000

    Fix vertex setter in messenger

--- src/core/messenger.hh
+++ src/core/messenger.hh
@@ -16,6 +16,9 @@ struct messenger {
   void set_start_id  (u64);
   void set_calib_belt(std::string);
   void set_generator (std::string);
+  void set_vertex_x  (f64);
+  void set_vertex_y  (f64);
+  void set_vertex_z  (f64);
 
 private:
   sim_config& s_;
